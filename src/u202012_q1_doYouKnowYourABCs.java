86ca59fcf860c6ccfceaf2cc19f72c7c53c4de97
asdf1357531 <xiaole.lee2009@gmail.com>
2022-07-02 21:06:14 -0400

    completed 202012 q1

--- src/u202012_q1_doYouKnowYourABCs.java
+++ src/u202012_q1_doYouKnowYourABCs.java
@@ -1,9 +1,11 @@
 import java.util.*;
-
+/*
+http://usaco.org/index.php?page=viewproblem2&cpid=1059
+*/
 public class u202012_q1_doYouKnowYourABCs {
-    public static void getabc(int[] inputs){
-        int min = 2147483647;
-        int min2 = 2147483647;
+    /*public static void getabc(int[] inputs){
+        int min = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
         int max = 0;
         for(int i = 0; i < 7; i++){
             if(inputs[i] < min2){
@@ -17,10 +19,12 @@ public class u202012_q1_doYouKnowYourABCs {
             }
         }
         System.out.println(min + " " + min2 + " " + (max - min - min2));
-    }
+    }*/
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int[] inputs = {in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt()};
-        getabc(inputs);
+
+        Arrays.sort(inputs);
+        System.out.println(inputs[0] + " " + inputs[1] + " " + (inputs[6] - inputs[1] - inputs[0]));
     }
 }
