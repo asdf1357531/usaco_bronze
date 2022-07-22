6e9be621dead0603ea205e2a5d30487985afa96c
asdf1357531 <xiaole.lee2009@gmail.com>
2022-07-21 22:15:23 -0400

    youtube solution

--- src/u202003_q1_socialDistancing1.java
+++ src/u202003_q1_socialDistancing1.java
@@ -1,31 +1,53 @@
 import java.util.*;
-public class u202003_q1_socialDistancing1 {
-    public static void main(String[] args) {
+import java.io.*;
+
+class socialDistancing1 {
+    public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         String s = in.next();
-        int d = 1;
-        int ld = 2147483647;
-        int hd1 = 0;
-        int hd2 = 0;
-        for(int i = 1; i < n; i++){
-            if(s.charAt(i) == '1'){
-                if(d > hd1){
-                    hd2 = hd1;
-                    hd1 = d;
-                } else if(d > hd2){hd2 = d;}
-                if(d < ld){ld = d;}
-                d = 0;
+        int d = 100000;
+        int hd1 = 1;
+        int hd2 = 1;
+        int hda = 1;
+        int beg = -1;
+        for (int i = 0; i < n; i++) {
+            boolean cchar = s.charAt(i) == '1';
+            if (cchar) {
+                if(beg == -1) {
+                    hd1 = Math.max(hd1, i);
+                    hda = Math.max(hda, i/2);
+                    beg = i;
+                }
+                else {
+                    int j = (i-beg)/2;
+                    if (j >= hd1) {
+                        hd2 = hd1;
+                        hd1 = j;
+                    }
+                    else if (j > hd2) {
+                        hd2 = j;
+                    }
+                    hda = Math.max(hda, (i-beg)/3);
+                    d = Math.min(d, i-beg);
+                    beg = i;
+                }
             }
-            d++;
         }
-        if(hd1 >= 3 * ld) {
-            System.out.println(ld);
-        } else if(hd1 >= 2 * ld & hd2 >= 2 * ld) {
-            System.out.println(ld);
-        } else {
-            System.out.println(Math.round(hd2/2));
+        if (beg == -1) {
+            hda = Math.max(hda, n-1);
         }
-
+        else {
+            int j = n-beg-1;
+            if (j >= hd1) {
+                hd2 = hd1;
+                hd1 = j;
+            }
+            else if (j > hd2) {
+                hd2 = j;
+            }
+        }
+        hda = Math.max(hda, (n-beg-1)/2);
+        System.out.println("" + Math.min(Math.max(Math.min(hd1, hd2), hda), d));
     }
 }
---
+++ src/u202103_q2_acowdemia2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+//http://usaco.org/index.php?page=viewproblem2&cpid=1132
+
+public class u202103_q2_acowdemia2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        int n = in.nextInt();
+        ArrayList<String> names = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            names.add(in.next());
+        }
+        for(int i = 0; i < k; i++){
+
+        }
+    }
+}
